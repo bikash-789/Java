4a640bba18537e8139ba4c86b2232e14233b3708
Bikash Chauhan <chauhanbikash789@gmail.com>
2023-04-18 00:19:28 +0530

    Solved part-(a) of Q31

--- src/Q31.java
+++ src/Q31.java
@@ -1,5 +1,7 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Q31 {
@@ -18,11 +20,50 @@ public class Q31 {
                 }
                 x++;
             }
+            System.out.println("Written successfully!");
+            input.close();
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+
+        // Now read the file and count the frequency of each word
+        try{
+            Scanner input = new Scanner(new FileInputStream("./Bikash.txt"));
+            String str="";
+            while(input.hasNext())
+            {
+                str+=input.nextLine()+" ";
+            }
+            // Create an array to store string tokens
+            String strArr[] = str.split(" ");
+
+            // Create a hashMap
+            HashMap<String, Integer> freqCount = new HashMap<>();
+
+            // Store words into HashMap with corresponding frequency count
+            for(int i=0; i < strArr.length; i++)
+            {
+                if(freqCount.containsKey(strArr[i]))
+                {
+                    int k = freqCount.get(strArr[i]);
+                    freqCount.put(strArr[i], ++k);
+                }
+                else{
+                    freqCount.put(strArr[i], 1);
+                }
+            }
+            // Iterate over HashMap and print frequency of each word
+            freqCount.forEach((key, value)->{
+                System.out.println(key + ": "+value);
+            });
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
 
